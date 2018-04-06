990b255ea5ca97b18178be3686f8a0808f859140
David Granjon <davidgranjon@imac-de-david.home>
2018-04-06 23:21:27 +0200

    update shinySliders according to the pull request in shinyWidgets.

--- method1_from_ui.R
+++ method1_from_ui.R
@@ -1,112 +1,71 @@
-
-
-# Method 1 : choose skin in UI --------------------------------------------
-
-# In this method, we define a skin in the UI
-# This apply to all sliders in the app
-
-# Todo : instead of recreating a complete dependency, use shiny's dep since all skins are available
-# https://github.com/rstudio/shiny/tree/master/inst/www/shared/ionrangeslider/css
-
-
-
-
-# Fun ---------------------------------------------------------------------
-
-# # new function similar to bootstrapLib() of shiny
-# # we use only shiny dependencies
-# ionrangesliderLib <- function (skin) {
-#   htmltools::htmlDependency(
-#     "ionrangeslider", "2.1.6", 
-#     c(href = "shared/ionrangeslider", 
-#       file = system.file("www/shared/ionrangeslider", package = "shiny")
-#     ), 
-#     script = "js/ionrangeslider.min.js", 
-#     stylesheet = c(
-#       "css/ion.rangeSlider.css",
-#       paste0("css/ion.rangeSlider.skin", skin, ".css"))
-#   )
-# }
-
-sliderInputDep <- function(skin) {
-  # recovers the dependencies 
-  # of a normal sliderInput
-  deps <- htmltools::findDependencies(
-    sliderInput(
-      inputId = "test", 
-      label = "", 
-      min = 0, max = 1000, value = 500
-    )
-  )
-  # replace the css skin by what the user want
-  # in chooseSliderSkin()
-  deps[[1]]$stylesheet[[2]] <- paste0("css/ion.rangeSlider.skin", skin, ".css")
-  return (deps)
-}
-
-chooseSliderSkin <- function(skin = c("Shiny", "Flat", "Modern", "Nice",
-                                      "Simple", "HTML5", "Round", "Square")) {
-  skin <- match.arg(arg = skin)
-  tagList(
-    #htmltools::suppressDependencies("ionrangeslider"),
-    htmltools::attachDependencies(
-      x = tags$div(),
-      value = sliderInputDep(skin), 
-      append = FALSE
-    )
-  )
-}
-
-# you can pick colors from here
-# https://www.w3schools.com/colors/colors_names.asp
-# sliderId can be a vector like c(0, 1, 2, ...)
-# depending on what slider you want to
-# customize
-# color is also a vector depending on the number of sliders
-setSliderColor <- function(color, sliderId) {
-  
-  # create custom css background for each slider
-  # start from 0 so need to use i-1 for slider Id
-  # instead of i
-  sliderCol <- lapply(1:length(sliderId), FUN = function(i) {
-      paste0(".js-irs-", i-1, " .irs-single,", 
-             " .js-irs-", i-1, " .irs-bar-edge,",
-             " .js-irs-", i-1, " .irs-bar{
-             background: ", color[i], ";
-  }"
-      )
-  })
-  
-  # insert this custom css code in the head
-  # of the shiy app
-  custom_head <- tags$head(tags$style(HTML(as.character(sliderCol))))
-  return(custom_head)
-}
-
-
-
-
-# Demo --------------------------------------------------------------------
-
+library(shiny)
+library(shinyWidgets)
+source("chooseSliderSkin.R")
+source("setSliderColor.R")
 
 ui <- fluidPage(
   
-  setSliderColor(c("yellow", "purple"), c(0, 1)),
-  chooseSliderSkin("Modern"),
-  sliderInput("obs", "Number of observations:",
-              min = 0, max = 1000, value = 500
+  title = "setSliderColor/chooseSliderSkin demo",
+  
+  setSliderColor(c(
+    "Aquamarine",
+    "Coral",
+    "Crimson", 
+    "DeepPink ", 
+    "Gold",
+    "YellowGreen",
+    "Orange",
+    "Peru",
+    "SlateBlue"
+  ), c(1:9)),
+  
+  chooseSliderSkin("Flat"),
+  
+  fluidRow(
+    column(
+      4,
+      sliderInput("obs1", "Slider Aquamarine Flat design:",
+                  min = 0, max = 1000, value = 500
+      ),
+      sliderInput("obs2", "Slider Coral Flat design:",
+                  min = 0, max = 1000, value = 500
+      ),
+      sliderInput("obs3", "Slider Crimson Flat design:",
+                  min = 0, max = 1000, value = 500
+      )
+    ),
+    column(
+      4,
+      sliderInput("obs4", "Slider DeepPink Flat design:",
+                  min = 0, max = 1000, value = 500
+      ),
+      sliderInput("obs5", "Slider Gold Flat design:",
+                  min = 0, max = 1000, value = 500
+      ),
+      sliderInput("obs6", "Slider YellowGreen Flat design:",
+                  min = 0, max = 1000, value = 500
+      )
+    ),
+    column(
+      4,
+      sliderInput("obs7", "Slider Orange Flat design:",
+                  min = 0, max = 1000, value = 500
+      ),
+      sliderInput("obs8", "Slider Peru Flat design:",
+                  min = 0, max = 1000, value = 500
+      ),
+      sliderInput("obs9", "Basic SlateBlue Flat design:",
+                  min = 0, max = 1000, value = 500
+      )
+    )
   ),
-  sliderInput("obs2", "Number of observations:",
-              min = 0, max = 1000, value = 500
-  ),
+  
   plotOutput("distPlot")
 )
 
-server <- function(input, output) {
+server <- function(input, output, session) {
   
-  output$distPlot <- renderPlot({
-    hist(rnorm(input$obs))
-  })
 }
 
 shinyApp(ui, server)
+
