a01b231df9074ac7a54f903857ba88f161b9839d
David Granjon <davidgranjon@imac-de-david.home>
2018-04-07 20:23:16 +0200

    improve the demo app by using all css colors.

--- method1_from_ui.R
+++ method1_from_ui.R
@@ -2,65 +2,38 @@ library(shiny)
 library(shinyWidgets)
 source("chooseSliderSkin.R")
 source("setSliderColor.R")
+source("slider_colors.R")
 
 ui <- fluidPage(
   
   title = "setSliderColor/chooseSliderSkin demo",
   
-  setSliderColor(c(
-    "Aquamarine",
-    "Coral",
-    "Crimson", 
-    "DeepPink ", 
-    "Gold",
-    "YellowGreen",
-    "Orange",
-    "Peru",
-    "SlateBlue"
-  ), c(1:9)),
+  titlePanel("Pimp your shiny sliders!"),
+  
+  setSliderColor(slider_colors, 1:length(slider_colors)),
   
   chooseSliderSkin("Flat"),
   
-  fluidRow(
-    column(
-      4,
-      sliderInput("obs1", "Slider Aquamarine Flat design:",
-                  min = 0, max = 1000, value = 500
-      ),
-      sliderInput("obs2", "Slider Coral Flat design:",
-                  min = 0, max = 1000, value = 500
-      ),
-      sliderInput("obs3", "Slider Crimson Flat design:",
-                  min = 0, max = 1000, value = 500
-      )
-    ),
-    column(
-      4,
-      sliderInput("obs4", "Slider DeepPink Flat design:",
-                  min = 0, max = 1000, value = 500
-      ),
-      sliderInput("obs5", "Slider Gold Flat design:",
-                  min = 0, max = 1000, value = 500
-      ),
-      sliderInput("obs6", "Slider YellowGreen Flat design:",
-                  min = 0, max = 1000, value = 500
-      )
-    ),
-    column(
-      4,
-      sliderInput("obs7", "Slider Orange Flat design:",
-                  min = 0, max = 1000, value = 500
-      ),
-      sliderInput("obs8", "Slider Peru Flat design:",
-                  min = 0, max = 1000, value = 500
-      ),
-      sliderInput("obs9", "Basic SlateBlue Flat design:",
-                  min = 0, max = 1000, value = 500
-      )
-    )
-  ),
+  br(), hr(),
   
-  plotOutput("distPlot")
+  # create a set of 148 sliders
+  # to test the 148 css colors
+  fluidRow(
+    lapply(0:3, FUN = function(j) {
+      column(
+        width = 3,
+        lapply((1:37) + 37 * j, FUN = function(i) {
+          tagList(
+            sliderInput(
+              inputId = paste0("slider", i), 
+              label = paste("Slider", slider_colors[i], "Flat design:"),
+              min = 0, max = 1, value = 0.5
+            )
+          )
+        })
+      )
+    })
+  )
 )
 
 server <- function(input, output, session) {
